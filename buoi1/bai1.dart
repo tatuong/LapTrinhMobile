3c0036fc5b51aa0446e331c5ffc9387834808d9c
tatuong <tatuong16@gmail.com>
2025-04-22 11:02:31 +0700

    Bang cuu chuong

---
+++ buoi1/bai1.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    print("Bang cuu chuong $i: ");
+    for (int j = 1; j <= 10; j++) {
+      print('$i * $j= ${i * j}');
+    }
+    print('');
+  }
+}
